79a1945b118ad7369e63ff1be4c893b01d22677d
kusuml04 <KK670@myscc.ca>
2023-03-31 12:30:55 -0400

    modify the ISP_Call.cs

--- KusumlataBooks.DataAccess/Repository/IRepository/ISP_Call.cs
+++ KusumlataBooks.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace KusumlataBooks.DataAccess.Repository.IRepository
 {
-    internal interface ISP_Call
+    public interface ISP_Call : IDisposable
     {
+        //e.g. first column of first row in the result set 
+        T Single<T>(string procedurename, DynamicParameters param = null);
+        //execute something to the database but not retrieve anything 
+        void Execute(string procedurename, DynamicParameters param = null);
+        //retrieves the complete row or record
+        T OneRecord<T>(string procedurename, DynamicParameters param = null);
+        //get all of the rows
+        IEnumerable<T> List<T>(string procedurename, DynamicParameters param = null);
+        //stored proceedure that returns two tables
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedurename, DynamicParameters param = null);
+
     }
 }
